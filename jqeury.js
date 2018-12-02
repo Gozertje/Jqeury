f5ea49fd430bd43ce9aad663529498cbff03ce15
Gozertje <31706719+Gozertje@users.noreply.github.com>
2018-12-02 17:36:16 +0100

    l

--- jqeury.js
+++ jqeury.js
@@ -3,3 +3,9 @@ $(document).ready(function(){
         $("h2").toggle();
     });
 });
+s
+$(document).ready(function(){
+    $("button1").click(function(){
+        $("div").animate({left: '250px'});
+    });
+});
